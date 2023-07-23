6b50bbe32160b8cc2c9499661e696a3f109a1ada
Mark Greenwood <mark.greenwood@measurabl.com>
2023-07-22 17:18:00 -0700

    Installed some packages, started on tests

--- test/database.test.ts
+++ test/database.test.ts
@@ -1,14 +1,42 @@
+import {
+  PostgreSqlContainer,
+  StartedPostgreSqlContainer,
+} from "testcontainers";
+import { Pool } from "pg";
+
 describe("The database", () => {
-  beforeEach(() => {
+  jest.setTimeout(10000);
+
+  let dbContainer: StartedPostgreSqlContainer;
+  let pool: Pool;
+
+  const testDbConfig = {
+    user: "postgres",
+    password: "postgres",
+    database: "postgres",
+  };
+
+  beforeEach(async () => {
     console.log("Before...");
+    dbContainer = await new PostgreSqlContainer()
+      .withUsername("postgres")
+      .withPassword("postgres")
+      .withDatabase("postgres")
+      .start();
+    pool = new Pool({
+      ...testDbConfig,
+      port: dbContainer.getMappedPort(5432),
+    });
   });
 
   afterEach(() => {
     console.log("After...");
   });
 
-  it("spins up and responds to an SQL command", () => {
-    const records = [];
-    expect(records.length).toBe(1);
+  it("spins up and responds to an SQL command", async () => {
+    const records = await pool.query<{ pointId: string }>(
+      "SELECT * FROM public.point",
+    );
+    expect(records.rows.length).toBe(1);
   });
 });
