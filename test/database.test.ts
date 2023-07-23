4ac2be165aa234d2ee79c1bf57ca8918ba1b59cb
Mark Greenwood <mrkgreenwood@gmail.com>
2023-07-23 07:56:26 -0700

    Debugging run migrations with flyway container

--- test/database.test.ts
+++ test/database.test.ts
@@ -1,12 +1,13 @@
 import {
   GenericContainer,
+  Network,
   PostgreSqlContainer,
   StartedPostgreSqlContainer,
   StartedTestContainer,
   Wait,
 } from "testcontainers";
 import { Pool } from "pg";
-import path from "path";
+import * as path from "path";
 
 describe("The database", () => {
   jest.setTimeout(30000);
@@ -22,8 +23,13 @@ describe("The database", () => {
 
   beforeEach(async () => {
     console.log("Before...");
+
+    // TODO: Do I need to create a Docker network for these two containers?
+    const network = await new Network().start();
+
     // Create the Postgres container
     dbContainer = await new PostgreSqlContainer()
+      .withNetwork(network)
       .withDefaultLogDriver()
       .withExposedPorts(5432)
       .withWaitStrategy(
@@ -35,22 +41,38 @@ describe("The database", () => {
       .withStartupTimeout(180000)
       .start();
 
+    // const dbLogs = await dbContainer.logs();
+    // dbLogs.on("data", console.log);
+
+    const dbContainerIp = dbContainer.getIpAddress(network.getName());
+    const dbContainerPort = dbContainer.getPort();
+
     // Get the container IP for Postgres DB
-    const databaseContainerIp = await getContainerActualIp(dbContainer);
-    if (!databaseContainerIp) {
-      await dbContainer.stop({ removeVolumes: true });
-      throw new Error("Unable to determine test Postgres container IP address");
-    }
+    // const databaseContainerIp = await getContainerActualIp(dbContainer);
+    // if (!databaseContainerIp) {
+    //   await dbContainer.stop({ removeVolumes: true });
+    //   throw new Error("Unable to determine test Postgres container IP address");
+    // }
     const migrationPath = path.resolve("migrations");
-    const migrateCommand = "";
+    // -url=jdbc:postgresql://test-db:5432/postgres -schemas=public -user=postgres -password=postgres -connectRetries=60 migrate
+    const migrateCommand = `flyway -url=jdbc:postgresql://${dbContainerIp}:${dbContainerPort}/postgres -schemas=public -user=postgres -password=postgres migrate`;
     // Use a generic container for Flyway (migrations)
-    await new GenericContainer("flyway/flyway:7")
-      .withDefaultLogDriver()
+    const flywayContainer = await new GenericContainer("flyway/flyway:7")
+      .withNetwork(network)
       .withWaitStrategy(Wait.forLogMessage("Successfully applied"))
+      .withDefaultLogDriver()
       .withCommand([migrateCommand])
-      .withBindMounts([{ source: migrationPath, target: "/flyway/sql" }])
+        // Use instead of .withBindMounts (no longer recommended)
+      .withCopyDirectoriesToContainer([
+        { source: migrationPath, target: "/flyway/sql" },
+      ])
       .withStartupTimeout(180000)
       .start();
+
+    console.log("Flyway container started...");
+
+    // const migrationLogs = await flywayContainer.logs();
+    // migrationLogs.on("data", console.log);
 
     // Get a connection pool to the Postgres container
     pool = new Pool({
@@ -65,7 +87,7 @@ describe("The database", () => {
 
   it("spins up and responds to an SQL command", async () => {
     const records = await pool.query<{ pointId: string }>(
-      "SELECT * FROM public.point",
+      "SELECT * FROM public.test_item",
     );
     expect(records.rows.length).toBe(1);
   });
@@ -73,6 +95,9 @@ describe("The database", () => {
 
 const getContainerActualIp = async (container: StartedTestContainer) => {
   const getIpCmd = await container.exec(["hostname", "-I"]);
+  console.log(getIpCmd);
+  const logs = await container.logs();
+  logs.on("data", console.log);
   const matches = /\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b/.exec(
     getIpCmd.output.trim(),
   );
