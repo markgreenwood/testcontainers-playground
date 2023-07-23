ba6b0c5541621de5a4f5fb94dee5feb07199325e
Mark Greenwood <mrkgreenwood@gmail.com>
2023-07-23 07:01:46 -0700

    Trying to implement migrations via testcontainers

--- test/database.test.ts
+++ test/database.test.ts
@@ -1,8 +1,12 @@
 import {
+  GenericContainer,
   PostgreSqlContainer,
   StartedPostgreSqlContainer,
+  StartedTestContainer,
+  Wait,
 } from "testcontainers";
 import { Pool } from "pg";
+import path from "path";
 
 describe("The database", () => {
   jest.setTimeout(30000);
@@ -18,11 +22,37 @@ describe("The database", () => {
 
   beforeEach(async () => {
     console.log("Before...");
+    // Create the Postgres container
     dbContainer = await new PostgreSqlContainer()
+      .withDefaultLogDriver()
+      .withExposedPorts(5432)
+      .withWaitStrategy(
+        Wait.forLogMessage("database system is ready to accept connections"),
+      )
       .withUsername("postgres")
       .withPassword("postgres")
       .withDatabase("postgres")
+      .withStartupTimeout(180000)
       .start();
+
+    // Get the container IP for Postgres DB
+    const databaseContainerIp = await getContainerActualIp(dbContainer);
+    if (!databaseContainerIp) {
+      await dbContainer.stop({ removeVolumes: true });
+      throw new Error("Unable to determine test Postgres container IP address");
+    }
+    const migrationPath = path.resolve("migrations");
+    const migrateCommand = "";
+    // Use a generic container for Flyway (migrations)
+    await new GenericContainer("flyway/flyway:7")
+      .withDefaultLogDriver()
+      .withWaitStrategy(Wait.forLogMessage("Successfully applied"))
+      .withCommand([migrateCommand])
+      .withBindMounts([{ source: migrationPath, target: "/flyway/sql" }])
+      .withStartupTimeout(180000)
+      .start();
+
+    // Get a connection pool to the Postgres container
     pool = new Pool({
       ...testDbConfig,
       port: dbContainer.getMappedPort(5432),
@@ -40,3 +70,11 @@ describe("The database", () => {
     expect(records.rows.length).toBe(1);
   });
 });
+
+const getContainerActualIp = async (container: StartedTestContainer) => {
+  const getIpCmd = await container.exec(["hostname", "-I"]);
+  const matches = /\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b/.exec(
+    getIpCmd.output.trim(),
+  );
+  return matches ? matches[0] : undefined;
+};
